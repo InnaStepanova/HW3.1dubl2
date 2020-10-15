e9cc336d91e0a1b5ab15f3d99b77ee5ad0a29ccf
Inna Stepanova <inna-panichkina@yandex.ru>
2020-10-15 15:25:15 +0300

    Initial Commit

---
+++ HW3.1dubl2/HW3_1dubl2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1dubl2App.swift
+//  HW3.1dubl2
+//
+//  Created by Инна Степанова on 15.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1dubl2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HW3.1dubl2Tests/HW3_1dubl2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  HW3_1dubl2Tests.swift
+//  HW3.1dubl2Tests
+//
+//  Created by Инна Степанова on 15.10.2020.
+//
+
+import XCTest
+@testable import HW3_1dubl2
+
+class HW3_1dubl2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
